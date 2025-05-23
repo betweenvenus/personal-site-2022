8c07b910d17ea0c4c34e4037a6eb102537f99088
Jon Highsmith <jon@gojira.dev>
2025-05-23 16:08:27 -0400

    Update for 2025

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,15 +13,9 @@
 	<h2>
 		I'm a <b>web developer</b> from Atlanta 
 		<img class="stamp" src="/peach.gif" alt="A delicious peach.">, 
-		home of the chicken wing <img class="stamp" src="/lemon-pepper.gif" alt="A delicious chicken wing.">. I currently work at 
-		<a 
-			href="https://orion.com"
-			target="_blank"
-			>
-			Orion Advisor Solutions</a>.
-		<img class="stamp" src="/bullish.gif" alt="A red arrow flying upward, resembling a financial performance chart.">
+		home of the chicken wing <img class="stamp" src="/lemon-pepper.gif" alt="A delicious chicken wing.">.
 	</h2>
-	<h2><b><a target="_blank" href="/jon-highsmith-resume-summer-2022.pdf">Download my resume here.</a></b></h2>
+	<h2>Need a good dev? <b><a href="mailto:jon@gojira.dev">Shoot me an e-mail!</a></b></h2>
 </section>
 
 <style>
